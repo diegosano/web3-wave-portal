dbe815ff2067413cf97ab3e96d1354e55d9392e0
Diego Sano <diego.sano@nave.rs>
2022-08-12 16:58:48 -0300

    feat: create loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css'
+
+export function Loader() {
+  return (
+    <div className={styles.ldsRoller}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
