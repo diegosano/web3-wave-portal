f104a36a02addf024db32bc38f89f542e473d7c0
Diego Sano <diego.sano@nave.rs>
2022-08-12 15:59:36 -0300

    feat: check if wallet is connected on load dapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
 import styles from './App.module.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  useEffect(() => {
+    const checkIfWalletIsConnected = async () => {
+      try {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+          console.log('Make sure you have metamask!');
+          return;
+        }
+
+        console.log('We have the ethereum object', ethereum);
+
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (
+          !accounts ||
+          !Array.isArray(accounts) ||
+          (Array.isArray(accounts) && !accounts.length)
+        ) {
+          console.log('No authorized account found');
+          return;
+        }
+
+        const account = accounts[0];
+        setCurrentAccount(account);
+        console.log('Found an authorized account:', account);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    checkIfWalletIsConnected();
+  }, []);
+
   const wave = () => {};
 
   return (
